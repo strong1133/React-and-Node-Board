ff799147a8dbfdbc931d42dc42859c1bcefb72f8
[strong1133] <strong1133@naver.com>
2021-10-18 23:56:42 +0900

    네비박스 생성

--- front/board/src/components/Header.js
+++ front/board/src/components/Header.js
@@ -1,3 +1,33 @@
 import React, {useEffect} from 'react';
 import styled , {css} from "styled-components"
 
+
+const Header = () =>{
+    return(
+        <Wrapper>
+            <NavBox>
+                네비박스
+            </NavBox>
+        </Wrapper>
+    )
+}
+
+export default Header;
+
+const Wrapper = styled.div`
+    width: 100%;
+    height:100%;
+    position: sticky;
+    top:0;
+    left:0;
+    margin: 0;
+    background-color: white;
+`;
+
+const NavBox = styled.div`
+    width: 100%;
+    height: 40px;
+
+    background-color: wheat;
+    text-align: center;
+`;
